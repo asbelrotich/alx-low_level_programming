0233b09e0d6300f95c73d462da628325b95413e6
Asbel-software-engineer <asbelkipkosgei0@gmail.com>
2023-07-24 13:48:51 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints a Fizz Buzz program
- * Return: Always 0 (Success)
+ *main - Prints a Fizz Buzz program
+ *Return: Always 0 (Success)
  */
 
 int main(void)
