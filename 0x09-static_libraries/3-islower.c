05952fa4b9ac86c82bd9ad38d4bcd5c3d230d940
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-01-23 08:19:53 -0800

    static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 if character is lowercase or  0 if anything else
+ */
+int _islower(int c)
+{
+	if  (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+}
+_putchar('\n');
+}
